82536b4bc5a5b0ed733ba84a9bf818eb22528487
Robert Abreu <robertabreu@pursuit.org>
2019-08-10 00:41:21 -0400

    created a custom interface of type Person for strongly typed person objects

--- index.ts
+++ index.ts
@@ -4,8 +4,23 @@ const url = new URL("...");
 const lucky = 100; // implicit declaration
 const oneHundred: number = lucky;  // explicitly 
 
-
+// create a custom type
 type Style = 'bold' | 'italic';
 
 let font: Style;
 
+// create custom template to strongly type an object
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+const person: Person = {
+    firstName: "Paul",
+    lastName: "Miller",
+}
+
+const person2: Person = {
+    firstName: "Allen",
+    lastName: "Roberts",
+}
