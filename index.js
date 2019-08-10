6671bead4fa190c00fb9934687f827685ecbfbf0
Robert Abreu <robertabreu@pursuit.org>
2019-08-10 00:56:50 -0400

    compiled ts to js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 console.log("hello world");
+// const url = new URL("...");
+var lucky = 100; // implicit declaration
+var oneHundred = lucky; // explicitly 
+var font;
+var person = {
+    firstName: "Paul",
+    lastName: "Miller"
+};
+var person2 = {
+    firstName: "Allen",
+    lastName: "Roberts"
+};
+// variables followed by their types && the return type at the end ":string"
+function pow(x, y) {
+    return Math.pow(x, y).toString();
+}
+;
+// this function returns nothing, set return type to void
+function logMe(message) {
+    console.log(message);
+}
+;
+// strongly typed array 
+var arr = [];
+var tuple = [];
+tuple.push("string");
